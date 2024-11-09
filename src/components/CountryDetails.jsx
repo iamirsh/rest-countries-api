eabd7fe5f3d1796094ec6dc42c7d7a0f691351dc
Irshad Ahmad <ahmadirshad96@gmail.com>
2024-11-09 19:36:15 +0530

    add border countries

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,56 +1,74 @@
 import React, { useEffect, useState } from "react";
 import { FaArrowLeft } from "react-icons/fa6";
 import "./CountryDetail.css";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const CountryDetails = () => {
-  const params = useParams();
-  const countryName = params.country;
-  const [countryData, setCountryData] = useState({});
-  const [notFound, setNotFound] = useState(false);
+  const params = useParams()
+  const countryName = params.country
+
+  const [countryData, setCountryData] = useState(null)
+  const [notFound, setNotFound] = useState(false)
 
   useEffect(() => {
-    const fetchCountryDetails = async () => {
-      try {
-        const response = await fetch(
-          `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
-        );
-        const [data] = await response.json();
+    fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+      .then((res) => res.json())
+      .then(([data]) => {
         setCountryData({
           name: data.name.common,
           nativeName: Object.values(data.name.nativeName)[0].common,
-          img: data.flags.png,
           population: data.population,
           region: data.region,
           subregion: data.subregion,
           capital: data.capital,
+          flag: data.flags.svg,
           tld: data.tld,
-          languages: Object.values(data.languages),
-          currencies: Object.values(data.currencies).map(
-            (currency) => currency.name
-          ),
+          languages: Object.values(data.languages).join(", "),
+          currencies: Object.values(data.currencies)
+            .map((currency) => currency.name)
+            .join(", "),
+          borders: [],
         });
-      } catch (error) {
+
+        if (!data.borders) {
+          data.borders = [];
+        }
+
+        Promise.all(
+          data.borders.map((border) => {
+            return fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+              .then((res) => res.json())
+              .then(([borderCountry]) => borderCountry.name.common);
+          })
+        ).then((borders) => {
+          setCountryData((prevState) => ({ ...prevState, borders }));
+        });
+      })
+      .catch((err) => {
+        console.log(err);
         setNotFound(true);
-      }
-    };
-    fetchCountryDetails();
-  }, []);
+      });
+  }, [countryName]);
 
   if (notFound) {
     return <div>Country Not Found</div>;
   }
 
-  return (
+  return countryData === null ? (
+    "loading..."
+  ) : (
     <main>
       <div className="max-w-[1200px] mx-auto mt-8">
-        <span className="back-button flex items-center gap-2 bg-gray-100 w-20 p-2 rounded-lg cursor-pointer" onClick={()=> history.back()}>
+        <span
+          className="back-button flex items-center gap-2 bg-gray-100 w-20 p-2 rounded-lg cursor-pointer"
+          onClick={() => history.back()}
+        >
           <FaArrowLeft />
           Back
         </span>
         <div className="flex flex-col items-start mt-8 gap-4 md:flex-row md:items-center md:gap-16 md:mt-16 ">
           <img
-            src={countryData.img}
+            src={countryData.flag}
             className="max-w-[400px] w-[100%] md:w-[40%] border-2 border-gray-100 rounded-lg"
             alt="flag"
           />
@@ -90,9 +108,16 @@ const CountryDetails = () => {
                 <span className="languages"></span>
               </p>
             </div>
-            <div className="border-countries">
-              <b>Border Countries: </b>&nbsp;
-            </div>
+            {countryData.borders.length !== 0 && (
+              <div className="border-countries">
+                <b>Border Countries: </b>&nbsp;
+                {countryData.borders.map((border) => (
+                  <Link key={border} to={`/${border}`}>
+                    {border}
+                  </Link>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
